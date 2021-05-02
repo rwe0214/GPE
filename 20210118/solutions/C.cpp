404cae0e8a3aa9987cb562d4f5f84ee15b004ff5
rwechiu <shwchiu@cs.nctu.edu.tw>
2021-05-03 00:49:48 +0800

    Update solutions/C.cpp of 20210118
    Using Dijkstra algorithm instead of Floyd-Warshall algorithm

--- 20210118/solutions/C.cpp
+++ 20210118/solutions/C.cpp
@@ -1,32 +1,51 @@
 #include <algorithm>
 #include <iostream>
-#include <cstring>
+#include <queue>
+#include <climits>
+#include <vector>
 
 using namespace std;
 
 int main(){
 	int n, m, case_num = 1;
 	while(cin >> n >> m && n!=0 && m!=0){
-		int limit[n][n];
-		memset(limit, 0, sizeof(limit));
+		vector<int> cap(n, -1);
+		vector<bool> visit(n, false);
+		vector<vector<int> > adj(n, vector<int>());
+		vector<vector<int> > weight(n, vector<int>(n, 0));
 
 		for(int i=0; i<m; i++){
 			int u, v, w;
 			cin >> u >> v >> w;
 			u--; v--;
-			limit[u][v] = limit[v][u] = w;
+			weight[u][v] =weight[v][u] = w;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
 		}
-
-		for(int k=0; k<n; k++)
-			for(int i=0; i<n; i++)
-				for(int j=0; j<n; j++)
-					limit[i][j] = max(limit[i][j], min(limit[i][k], limit[k][j]));
 		
 		int s, d, t;
 		cin >> s >> d >> t;
 		s--; d--;
-		int cap = limit[s][d] - 1;
-		int times = (t%cap) ? t/cap+1 : t/cap;
+
+		priority_queue<pair<int, int>, vector<pair<int, int> >, less<pair<int, int> > > pq;
+		pq.push(make_pair(INT_MAX, s));
+		cap[s] = INT_MAX;
+
+		while(!pq.empty()){
+			int u = pq.top().second;
+			pq.pop();
+			if(visit[u])
+				continue;
+			visit[u] = true;
+			for(int i=0; i<adj[u].size(); i++){
+				int v = adj[u][i];
+				cap[v] = max(cap[v], min(cap[u], weight[u][v]));
+				pq.push(make_pair(cap[v], v));
+			}
+		}
+
+		cap[d] = cap[d] - 1;
+		int times = (t%cap[d]) ? t/cap[d]+1 : t/cap[d];
 		cout << "Scenario #" << case_num++ << endl;
 		cout << "Minimum Number of Trips = " << times << endl;
 		cout << endl;
