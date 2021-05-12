c411925c5625a49ab39318e3f431dbef57c7dd00
rwechiu <shwchiu@cs.nctu.edu.tw>
2021-05-12 23:37:53 +0800

    Fix solution/E.cpp of 20210118

--- 20210118/solutions/E.cpp
+++ 20210118/solutions/E.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace std;
@@ -7,33 +8,27 @@ using namespace std;
 int main() {
     int n;
     while (cin >> n && n != 0) {
-        vector<int> nums;
+        set<long long> nums;
         for (int i = 0; i < n; i++) {
-            int buf;
+            long long buf;
             cin >> buf;
-            nums.push_back(buf);
+            nums.insert(buf);
         }
-        sort(nums.begin(), nums.end());
-        int a, lo, hi, d;
         bool solved = false;
-        for (int i = n - 1; i >= 3 && !solved; i--) {
-            d = nums[i];
-            for (int j = 0; j < i && !solved; j++) {
-                a = nums[j];
-                lo = j + 1;
-                hi = i - 1;
-                while (lo < hi && !solved) {
-                    int test = a + nums[lo] + nums[hi];
-                    if (test == d) {
+        for (auto i = nums.rbegin(); i != nums.rend() && !solved; i++) {
+            for (auto j = nums.rbegin(); j != nums.rend() && !solved; j++) {
+                for (auto k = next(j); k != nums.rend() && !solved; k++) {
+                    int a = *i - *j - *k;
+                    if (*i == *j || *j == *k) continue;
+                    if (a == *i || a == *j || a == *k) continue;
+                    if (nums.count(a)) {
                         solved = true;
-                        cout << d << endl;
-                    } else if (test < d)
-                        lo++;
-                    else
-                        hi--;
+                        cout << *i << endl;
+                    }
                 }
             }
         }
+
         if (!solved) cout << "no solution" << endl;
     }
 
